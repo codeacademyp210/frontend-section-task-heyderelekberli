d82c700801091bb1ad63ddf1f075000420908ab2
heyderelekberli <HeydarRa@code.edu.az>
2019-04-13 11:32:56 +0400

    continue

--- js/script.js
+++ js/script.js
@@ -1,31 +1,40 @@
 "use strict";
 
-let mainSlider = document.querySelector(".mainSlider");
-let slide = Array.from(mainSlider.children);
-let prevButton = document.querySelector(".prevSlider");
-let nextButton = document.querySelector(".nextSlider");
+const mainSlayt = document.querySelector(".mainSlider");
+const slide = Array.from(mainSlayt.children);
+const prevButton = document.querySelector(".prevBtn");
+const nextButton = document.querySelector(".nextBtn");
 let itemSize = slide[0].getBoundingClientRect();
-let setSliderPosition = function(item, index){
+console.log(itemSize);
+const setSliderPosition = (item, index) => {
     item.style.left = index * itemSize.width + "px";
 };
 
 slide.forEach(setSliderPosition);
+
 prevButton.addEventListener("click", function (e) {
-    let show = mainSlider.querySelector(".showSlider");
-    if (show.previousElementSibling) {
-        let prevItem = show.previousElementSibling;
-        prevItem.classList.add("showSlider");
-        show.classList.remove("showSlider");
-        mainSlider.style.transform = "translateX(-" + prevItem.style.left + ")";
+    let showSlider = mainSlayt.querySelector(".show");
+    if (showSlider.previousElementSibling) {
+        let prev = showSlider.previousElementSibling;
+        prev.classList.add("show");
+        showSlider.classList.remove("show");
+
+        prev.querySelector(".slider-content").classList.add("animated", "bounceInLeft");
+        showSlider.querySelector(".slider-content").classList.remove("animated", "bounceInLeft", "bounceInRight");
+        mainSlayt.style.transform = "translateX(-" + prev.style.left + ")";
     }
 });
+// Click next button, slider move to the right
 nextButton.addEventListener("click", function (e) {
-    let show = mainSlider.querySelector(".showSlider");
-    console.log(show);
-    if (show.nextElementSibling) {
-        let next = show.nextElementSibling;
-        next.classList.add("showSlider");
-        show.classList.remove("showSlider");
-        mainSlider.style.transform = "translateX(-" + next.style.left + ")";
-    } 
+    let showSlider = mainSlayt.querySelector(".show");
+    console.log(showSlider);
+    if (showSlider.nextElementSibling) {
+        let next = showSlider.nextElementSibling;
+        next.classList.add("show");
+        showSlider.classList.remove("show");
+
+        next.querySelector(".slider-content").classList.add("animated", "bounceInRight");
+        showSlider.querySelector(".slider-content").classList.remove("animated", "bounceInRight", "bounceInLeft");
+        mainSlayt.style.transform = "translateX(-" + next.style.left + ")";
+    }
 });
