49d3567aef5a245447f4864ee2a66b577806ce4d
heyderelekberli <HeydarRa@code.edu.az>
2019-04-16 01:48:35 +0400

    done

--- js/script.js
+++ js/script.js
@@ -15,7 +15,6 @@ const slide = Array.from(mainSlayt.children);
 const prevButton = document.querySelector(".prevBtn");
 const nextButton = document.querySelector(".nextBtn");
 let itemSize = slide[0].getBoundingClientRect();
-console.log(itemSize);
 const setSliderPosition = (item, index) => {
     item.style.left = index * itemSize.width + "px";
 };
@@ -40,7 +39,6 @@ nextButton.addEventListener("click", function (e) {
         let next = showSlider.nextElementSibling;
         next.classList.add("show");
         showSlider.classList.remove("show");
-
         next.querySelector(".slider-content").classList.add("animated", "bounceInRight");
         showSlider.querySelector(".slider-content").classList.remove("animated", "bounceInRight", "bounceInLeft");
         mainSlayt.style.transform = "translateX(-" + next.style.left + ")";
